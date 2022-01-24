6302f46b981883c5d21986f433eb9ad11df930c5
knowdeep <worstgrim@gmail.com>
2022-01-24 21:57:47 +0700

    dasda

--- next.config.js
+++ next.config.js
@@ -5,5 +5,5 @@ module.exports = {
    * Tell Next.js where the `public` folder is.
    * Replace `nextjs-github-pages` with your Github repo project name.
    */
-  assetPrefix: isProd ? "/Johnbak-dsaddasdasdada/" : "",
+  assetPrefix: isProd ? "/Johnbak-github-dsaddasdasdada/" : "",
 };
