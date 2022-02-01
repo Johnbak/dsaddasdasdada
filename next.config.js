ef44b2bb0227a1e8c2f7cb3c369e3397db3a4930
knowdeep <worstgrim@gmail.com>
2022-02-01 22:27:49 +0700

    test

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/dsaddasdasdada-github-repo/' : '/dsaddasdasdada-github-repo/',
+  assetPrefix: isProd ? '/dsaddasdasdada/' : '/dsaddasdasdada/',
 };
